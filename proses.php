be504173430307b5c2b6554ba476720bbd06e997
unknown <ristaamt@gmail.com>
2023-03-28 12:02:47 +0700

    v1.3

--- proses.php
+++ proses.php
@@ -4,17 +4,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $angka2 = $_POST['angka2'];
   $operator = $_POST['operator'];
 
-  if ($operator === '+') {
-    $hasil = $angka1 + $angka2;
-  } else if ($operator === '-') {
-    $hasil = $angka1 - $angka2;
-  } else if ($operator === '*') {
-    $hasil = $angka1 * $angka2;
-  } else if ($operator === '/') {
-    $hasil = $angka1 / $angka2;
-  } else {
-    $hasil = "Operator tidak valid";
+  switch ($operator) {
+    case '+':
+      $hasil = $angka1 + $angka2;
+      break;
+    case '-':
+      $hasil = $angka1 - $angka2;
+      break;
+    case '*':
+      $hasil = $angka1 * $angka2;
+      break;
+    case '/':
+      $hasil = $angka1 / $angka2;
+      break;
+    default:
+      $hasil = "Operator tidak valid";
   }
 
   echo "Hasil dari " . $angka1 . " " . $operator . " " . $angka2 . " adalah " . $hasil;
 }
+?>
